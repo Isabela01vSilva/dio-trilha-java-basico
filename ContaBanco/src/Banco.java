059afc2d9b89d82af1cfd7436c182729430c6363
Isabela Silva <isabela.vsilva@bandtec.com.br>
2025-04-25 12:16:06 -0300

    git commit -m "feat: create and update classes"

---
+++ ContaBanco/src/Banco.java
@@ -0,0 +1,15 @@
+import java.util.List;
+
+public class Banco {
+
+    private String name;
+    private List<Conta> contas;
+
+    public String getName() {
+        return name;
+    }
+
+    public List<Conta> getContas() {
+        return contas;
+    }
+}
---
+++ ContaBanco/src/Cliente.java
@@ -0,0 +1,17 @@
+public class Cliente {
+
+    private String name;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    @Override
+    public String toString() {
+        return name;
+    }
+}
