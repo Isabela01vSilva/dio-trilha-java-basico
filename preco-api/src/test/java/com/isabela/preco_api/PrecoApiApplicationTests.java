71135ff8e945ed5c2720c51bf6f0dce587b92816
Isabela Silva <isabela.vsilva@bandtec.com.br>
2025-05-07 19:35:54 -0300

    Initial project setup

---
+++ preco-api/src/test/java/com/isabela/preco_api/PrecoApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.isabela.preco_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PrecoApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
